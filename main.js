f33fa5915c9d95147b53f40dde0f9c01e5a3f67e
Evelien Vanophalvens <Evelien.Vanophalvens@telenet.be>
2024-04-18 13:28:42 +0200

    Feat: centrale connectie

--- main.js
+++ main.js
@@ -1,36 +1,32 @@
 document.addEventListener('DOMContentLoaded', function() {
     document.querySelector("form").addEventListener('submit', function(event) {
         event.preventDefault();
-    
-        const longitude = parseFloat(document.querySelector('.longitude').value);
-        const latitude = parseFloat(document.querySelector('.latitude').value);
-        const extraInfo = document.querySelector('.extraInfo').value;
-    
-        // Controleer of de velden zijn ingevuld
-        if (!longitude || !latitude || !extraInfo) {
-            document.querySelector('.error').style.display = 'block';
-            return;
-        }
 
-    
-        fetch('https://pulsaidapi.onrender.com/api/v1/emergencies', {
-            method: 'POST',
-            mode: 'cors', 
-            headers: {
-                'Content-Type': 'application/json; charset=UTF-8',
-            },
-            body: JSON.stringify({ longitude, latitude, extraInfo })
+        let address = document.querySelector('#callLocation').value;
+        let extraInfo = document.querySelector('#callDescription').value;
+
+        fetch(`https://api.tomtom.com/search/2/geocode/${address}.json?key=9mi9TL6F8ECK0hqq36RFJ4xLAlDXwOok`)
+        .then(resp => resp.json())
+        .then((geocodingResult) => {
+            let longitude = geocodingResult['results'][0]['position']['lon'];
+            let latitude = geocodingResult['results'][0]['position']['lat'];
+
+            return fetch('https://pulsaidapi.onrender.com/api/v1/emergencies', {
+                method: 'POST',
+                mode: 'cors', 
+                headers: {
+                    'Content-Type': 'application/json; charset=UTF-8',
+                },
+                body: JSON.stringify({ longitude, latitude, extraInfo })
+            });
         })
         .then(response => response.json())
         .then(data => {
             if (data.status == 200) {
-                document.querySelector('.success').style.display = 'block';
             } else {
-                document.querySelector('.error').style.display = 'block';
             }
         })
         .catch(error => {
-            document.querySelector('.error').style.display = 'block';
         });
-    });
+    }, false);
 });
